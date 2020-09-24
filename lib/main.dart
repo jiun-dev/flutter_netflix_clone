f477c0f72d608bb98f1c0d13b2acfb99ba83bb2c
jiun Sin <jiunsin@jiunui-MacBookPro.local>
2020-09-24 19:24:57 +0900

    강의를 보며 클론코딩한 넷플릭스 앱

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'netflix',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white),
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics:
+                  NeverScrollableScrollPhysics(), // 손가락 움직임으로 스크롤 되는 것을 막는 기능
+              children: [
+                HomeScreen(),
+                Container(),
+                Container(),
+                Container(),
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          )),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/widget/carousel_slider.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            CarouselImage(
+              movies: movies,
+            ),
+            TopBar(),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            'images/bbongflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 컨텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
